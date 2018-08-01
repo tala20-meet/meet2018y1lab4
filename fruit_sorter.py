3829524ba50082edfc90db0e0b18888b1fe16751
tala20-meet <tala20@meet.mit.edu>
2018-08-01 09:58:59 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,8 @@
+new_fruit=input('what fruit am I sorting?')
+if new_fruit=="apples":
+	print('Bin 1')
+elif new_fruit=="oranges":
+	print('Bin 2')
+else:
+	print('ERROR! I do not recognize this fruit!')
+
